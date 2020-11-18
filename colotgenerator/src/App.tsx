39040a041ffdcc1c641907c288cd2b89a05ea5f9
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2020-11-18 13:38:51 +0500

    generator

--- colotgenerator/src/App.tsx
+++ colotgenerator/src/App.tsx
@@ -1,7 +1,47 @@
 import React from "react";
-
+import Values from "values.js";
+import { SingleColor } from "./Single";
 function App() {
-  return <div>hi dear</div>;
+  const [color, setColor] = React.useState("");
+  const [error, setError] = React.useState(false);
+  const [list, setList] = React.useState([]);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(0);
+      setList(colors);
+      console.log(colors);
+    } catch (error) {
+      setError(error);
+      console.log("hello", error);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setColor(e.target.value)
+            }
+            placeholder="#f15025"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn">onclick</button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          console.log(color, "color");
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
+      </section>
+    </>
+  );
 }
 
 export default App;
---
+++ colotgenerator/src/Single.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+interface tpProps {
+  rgb: string;
+  weight: string;
+  index: string;
+}
+export const SingleColor = ({ rgb, weight, index }: tpProps) => {
+  return <div>hi</div>;
+};
